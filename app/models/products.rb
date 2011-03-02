7a60f3f03716dcc1fecdb4da8a68984e74e00bdc
¨Eduardo <eduardo_2901@hotmail.com>
2011-03-02 16:39:54 -0600

    Ejercicio 2

--- app/models/products.rb
+++ app/models/products.rb
@@ -1,3 +1,7 @@
 class Products < ActiveRecord::Base
-validates_numericality_of :price
+	validates_numericality_of :price, :greater_than 0.01
+	validates_uniqueness_of :title
+	validates_presence_of :title, :description, :price, :image_url
+	validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i
+	validates_numericality_of :projection, :greater_than 0
 end
