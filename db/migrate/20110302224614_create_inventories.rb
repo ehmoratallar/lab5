acb376611dd1a39e7a7a27e4242349e87551814b
¨Eduardo <eduardo_2901@hotmail.com>
2011-03-03 17:25:59 -0600

    Commit

---
+++ db/migrate/20110302224614_create_inventories.rb
@@ -0,0 +1,18 @@
+class CreateInventories < ActiveRecord::Migration
+  def self.up
+    create_table :inventories , :primary_key => :id, :options => "auto_increment = 1" do |t|
+       
+       t.integer :id
+       t.integer :product_id
+       t.integer :previous_balance
+       t.integer :new_balance
+       t.integer :future_balance
+       
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :inventories
+  end
+end
